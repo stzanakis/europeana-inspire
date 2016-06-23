0579c0688d1a8018a965bc6fe1143ee7ec4264cd
stzanakis <simon.tzanakis@europeana.eu>
2016-06-23 13:21:31 +0200

    Update get response for boards with pretty json

--- src/main/java/eu/europeana/inspire/services/InspireServices.java
+++ src/main/java/eu/europeana/inspire/services/InspireServices.java
@@ -1,11 +1,15 @@
 package eu.europeana.inspire.services;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import eu.europeana.accessors.MeAccessor;
 import eu.europeana.common.Manager;
 import eu.europeana.exceptions.BadRequest;
 import eu.europeana.exceptions.DoesNotExistException;
 import org.apache.commons.configuration.ConfigurationException;
+import org.json.simple.JSONObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -19,7 +23,7 @@ import java.util.List;
  * @author Simon Tzanakis (Simon.Tzanakis@europeana.eu)
  * @since 2016-06-23
  */
-@Path("/inspire")
+@Path("/")
 public class InspireServices {
 
     @GET
@@ -42,8 +46,17 @@ public class InspireServices {
         MeAccessor meAccessor = Manager.accessorsManager.getMeAccessor();
         List<String> allMyBoardsInternalName = meAccessor.getAllMyBoardsInternalName();
 
-        String json = new Gson().toJson(allMyBoardsInternalName);
-        return json;
+        JSONObject obj = new JSONObject();
+        obj.put("table-count", new Integer(allMyBoardsInternalName.size()));
+        obj.put("names", allMyBoardsInternalName);
+
+        JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(obj.toString()).getAsJsonObject();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String prettyJson = gson.toJson(json);
+
+        return prettyJson;
     }
 
 }
